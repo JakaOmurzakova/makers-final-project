1a943f363646eb86922d6644635d681055dbe925
edilbek1507 <edilbek555@gmail.com>
2023-08-17 11:20:39 +0600

    fixes 10th

--- src/pages/PaymentPage.jsx
+++ src/pages/PaymentPage.jsx
@@ -1,7 +1,91 @@
-import React from "react";
+import * as React from "react";
+import Box from "@mui/material/Box";
+import IconButton from "@mui/material/IconButton";
+import Input from "@mui/material/Input";
+import FilledInput from "@mui/material/FilledInput";
+import OutlinedInput from "@mui/material/OutlinedInput";
+import InputLabel from "@mui/material/InputLabel";
+import InputAdornment from "@mui/material/InputAdornment";
+import FormHelperText from "@mui/material/FormHelperText";
+import FormControl from "@mui/material/FormControl";
+import TextField from "@mui/material/TextField";
+import Visibility from "@mui/icons-material/Visibility";
+import VisibilityOff from "@mui/icons-material/VisibilityOff";
 
-const PaymentPage = () => {
-  return <div>PaymentPage</div>;
-};
+export default function PaymentPage() {
+  const [showPassword, setShowPassword] = React.useState(false);
 
-export default PaymentPage;
+  const handleClickShowPassword = () => setShowPassword((show) => !show);
+
+  const handleMouseDownPassword = (event) => {
+    event.preventDefault();
+  };
+
+  return (
+    <Box
+      sx={{
+        display: "flex",
+        flexWrap: "wrap",
+        marginTop: "100px",
+        height: "70vh",
+        justifyContent: "center",
+        alignItems: "center",
+      }}
+    >
+      <div>
+        <h2 style={{ color: "whitesmoke" }}>Payment</h2>
+        <TextField
+          label="Payment"
+          id="standard-start-adornment"
+          sx={{ m: 1, width: "25ch" }}
+          InputProps={{
+            startAdornment: (
+              <InputAdornment position="start">Name</InputAdornment>
+            ),
+          }}
+          variant="standard"
+        />
+        <FormControl variant="standard" sx={{ m: 1, mt: 3, width: "25ch" }}>
+          <Input
+            id="standard-adornment-weight"
+            inputProps={{
+              "aria-label": "weight",
+            }}
+          />
+          <FormHelperText id="standard-weight-helper-text">
+            Email
+          </FormHelperText>
+        </FormControl>
+        <FormControl fullWidth sx={{ m: 1 }} variant="standard">
+          <InputLabel htmlFor="standard-adornment-amount">
+            Card Number
+          </InputLabel>
+          <Input
+            id="standard-adornment-amount"
+            startAdornment={
+              <InputAdornment position="start">Card Number</InputAdornment>
+            }
+          />
+        </FormControl>{" "}
+        <FormControl sx={{ m: 1, width: "25ch" }} variant="standard">
+          <InputLabel htmlFor="standard-adornment-password">CVV</InputLabel>
+          <Input
+            id="standard-adornment-password"
+            type={showPassword ? "text" : "password"}
+            endAdornment={
+              <InputAdornment position="end">
+                <IconButton
+                  aria-label="toggle password visibility"
+                  onClick={handleClickShowPassword}
+                  onMouseDown={handleMouseDownPassword}
+                >
+                  {showPassword ? <VisibilityOff /> : <Visibility />}
+                </IconButton>
+              </InputAdornment>
+            }
+          />
+        </FormControl>
+      </div>
+    </Box>
+  );
+}
